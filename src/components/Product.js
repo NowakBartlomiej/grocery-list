82ea517f9cddc4d56df07b4ad803dc3b5179ea37
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-05-29 13:54:28 +0200

    Created Product component

---
+++ src/components/Product.js
@@ -0,0 +1,83 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+import { Checkbox} from 'native-base'
+
+// Popup Menu
+import {Menu, MenuOption, MenuOptions, MenuTrigger} from 'react-native-popup-menu';
+
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Product() {
+  return (
+    <View style={styles.productBox}>
+        <Checkbox
+            isChecked={false}
+            onChange={() => {
+                console.log("Checkbox")
+            }}
+            value={"1"} accessibilityLabel="Checkbox" colorScheme="green" size="lg"
+        />
+
+        <View style={styles.textBox}>
+            <Text style={styles.productText}>Produkt</Text>
+
+            <Menu>
+                <MenuTrigger>
+                    <Ionicons name="ellipsis-vertical" size={26} color="#002317" />
+                </MenuTrigger> 
+                <MenuOptions optionsContainerStyle={{width: 95}}>
+                    
+                    <MenuOption 
+                    onSelect={() => console.log('USUN')} 
+                    >
+                    <Text>Usuń Listę</Text>
+                    </MenuOption>  
+
+                    <MenuOption 
+                    onSelect={() => console.log('EDIT')} 
+                    >
+                    <Text>Edytuj Listę</Text>
+                    </MenuOption> 
+
+                </MenuOptions> 
+            </Menu>
+        </View>
+
+
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    productBox: {
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      gap: 18,
+      padding: 10,
+      borderRadius: 10,
+      borderColor: '#00A57A',
+      borderWidth: 1.7,  
+      marginVertical: 6,
+    },
+  
+    textBox: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flex: 1
+    },
+  
+    productText: {
+      color: '#002317',
+      fontSize: 26,
+      flexShrink: 1,
+    },
+  
+    amountText: {
+      color: '#B1B1B0',
+      fontSize: 24,
+    },
+  });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 
-import { Header } from '../components'
+import { Header, Product } from '../components'
 
 export default function MainScreen() {
     return (
       <>
         <Header />
+
+        <ScrollView style={styles.scrollView}>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </ScrollView>
       </>
     )
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'F2F7FF', 
+    marginBottom: 60,
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+  
+  noListBox: {
+    marginTop: '50%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  noListText: {
+    fontSize: 40,
+    color: '#B1B1B0'
+  }
+});
